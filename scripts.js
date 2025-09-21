18089a465a84448097054ff6cc632917124032d6
Bielkzz <tomazigabriel3@gmail.com>
2025-09-20 22:55:49 -0300

    feat: Adicionar API para cotações de moedas ao vivo
    Adiciona a API "Awesomeapi" para buscar dados de cotações em tempo real.
    Essa mudança inclui:
    - Implementação da busca por cotações de USD, EUR e BTC.
    - Adição da funcionalidade de conversão para Bitcoin (BTC) e Libra Esterlina (GBP).
    - Refatoração do código para melhor organização das funções.

--- scripts.js
+++ scripts.js
@@ -5,43 +5,62 @@ const aviso = document.querySelector(".aviso")
 aviso.style.display = "none"
 
 
-function convertValues() {
+async function convertValues() {
     inputCurrencyValue = document.querySelector(".input-currency").value
     currencyValueToConvert = document.querySelector(".currency-value-to-convert")
     currencyValueConverted = document.querySelector(".currency-value")
 
-    const dolarToday = 5.2;
-    const euroToday = 6.2;
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+
+
+    const dolarToday = data.USDBRL.high;
+    const euroToday = data.EURBRL.high;
+    const bitcoinToday = data.BTCBRL.high;
+    const libraToday = 7.26;
+
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
 
-    if (currencySelect.value == "dolar") {
+    if (currencySelect.value === "dolar") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
         }).format(inputCurrencyValue / dolarToday)
-    }else if (currencySelect.value == "euro"){
+
+    } else if (currencySelect.value === "euro") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "EUR"
         }).format(inputCurrencyValue / euroToday)
-    }
 
+    } else if (currencySelect.value === "bitcoin") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'BTC',
+            minimumFractionDigits: 8
+        }).format(inputCurrencyValue / bitcoinToday)
+    } else if (currencySelect.value === "libra") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / libraToday)
+    }
 }
 
 
 function changeCurrency() {
     const currencyName = document.querySelector("#currency-name")
     const currencyImg = document.querySelector(".currency-img")
-    
 
-    if(currencyToSelect.value === currencySelect.value){
+
+    if (currencyToSelect.value === currencySelect.value) {
         aviso.style.display = "block"
-        
-    }else {
+
+    } else {
         aviso.style.display = "none"
     }
 
@@ -49,11 +68,19 @@ function changeCurrency() {
     if (currencySelect.value == "euro") {
         currencyName.innerHTML = "Euro"
         currencyImg.src = "./assets/euro.png"
-    }else if (currencySelect.value == "dolar") {
+    } else if (currencySelect.value == "dolar") {
         currencyName.innerHTML = "Dólar Americano"
         currencyImg.src = "./assets/dolar.png"
+    } else if (currencySelect.value == "bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./assets/bitcoin.png"
+    } else if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImg.src = "./assets/libra.png"
     }
     convertValues()
 
 
 }
+
+changeCurrency();
